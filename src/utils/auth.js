bc29debc1cd1042e7322905e773028ea1023a35c
adeirhmmm <ismayandalinda18@gmail.com>
2026-07-03 20:46:13 +0700

    fix: struktur supabase lengkap

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,7 +3,7 @@
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
-import { createClient } from './Supabase/server'
+import { createClient } from '../utils/supabase/server'
 
 export async function login(formData) {
   const supabase = await createClient()
